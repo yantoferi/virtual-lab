a10d04b0132f52d1d2248e278764796248e74ec0
feri <casterjam56@gmail.com>
2023-10-24 22:04:48 +0800

    perubahan interaksi screen

--- components/model/RoomTV.jsx
+++ components/model/RoomTV.jsx
@@ -4,27 +4,15 @@ Command: npx gltfjsx@6.2.13 /home/TA/resource/Laboratory/RoomTV.glb --transform 
 Files: /home/TA/resource/Laboratory/RoomTV.glb [2.28MB] > RoomTV-transformed.glb [378.62KB] (83%)
 */
 
-import { useGLTF, useVideoTexture } from '@react-three/drei'
+import { Html, useGLTF } from '@react-three/drei'
 import { useFrame } from '@react-three/fiber'
 import { Interactive } from '@react-three/xr'
-import { useState } from 'react'
 import { toast } from 'react-toastify'
+import Styles from '../../app/styles.module.css'
 
 export function RoomTV(props) {
   const { nodes, materials } = useGLTF('models/RoomTV-transformed.glb')
 
-  const [idVideo, setIdVideo] = useState(1)
-
-  const texture = useVideoTexture(`/videos/video_${idVideo}.mp4`, { loop: true })
-
-  const changeVideo = event => {
-    if ((event.distance <= 0.7) || (event.intersection?.distance <= 0.7)) {
-      if (idVideo === 4) {
-        return setIdVideo(1)
-      }
-      setIdVideo(idVideo + 1)
-    }
-  }
   useFrame(() => {
     // 
   }, 4)
@@ -39,10 +27,15 @@ export function RoomTV(props) {
       <mesh castShadow receiveShadow geometry={nodes.Cabinet_cover.geometry} material={materials['wood (Oak Matt Antracite Gray)']} position={[-0.214, 0.052, 2.173]} rotation={[-Math.PI, 0, -Math.PI]} scale={0.6} />
       <mesh castShadow receiveShadow geometry={nodes.Cabinet1.geometry} material={materials.ceramic} position={[0.326, 0.058, 2.284]} rotation={[-Math.PI, 0, -Math.PI]} scale={0.6} />
       <group position={[-0.214, 0.465, 2.185]} rotation={[-Math.PI, 0, -Math.PI]} scale={0.6}>
-        <Interactive onSelect={changeVideo}>
-          <mesh castShadow receiveShadow geometry={nodes.TV_1.geometry} onClick={changeVideo} rotation-z={-Math.PI}
-            onPointerEnter={event => { if (event.distance <= 0.7) toast.info('Klik untuk mengubah video!', { autoClose: 1000 }) }}>
-            <meshStandardMaterial map={texture} attach='material' />
+        <Interactive>
+          <mesh geometry={nodes.TV_1.geometry}
+            onPointerEnter={event => { if (event.distance <= 0.7) toast.info('Tekan esc untuk realese cursor', { autoClose: 1000 }) }}>
+            {/* <meshStandardMaterial map={texture} attach='material' /> */}
+            <Html className={Styles.content} transform scale={0.04} position-x={0.038} zIndexRange={[10010, 0]}>
+              <div className='w-full h-full text-black'>
+                <iframe src='https://itk.ac.id/' className='w-full h-full' title='ITK' />
+              </div>
+            </Html>
           </mesh>
         </Interactive>
         <mesh castShadow receiveShadow geometry={nodes.TV_2.geometry} material={materials.frame} />
