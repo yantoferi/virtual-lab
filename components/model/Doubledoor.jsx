f570a456b9fab1c99b04aef4444d2da5a7759ff1
feri <casterjam56@gmail.com>
2023-12-06 17:51:34 +0800

    change navigate

--- components/model/Doubledoor.jsx
+++ components/model/Doubledoor.jsx
@@ -39,7 +39,7 @@ export function Door(props) {
     myContext.changedest(props.destination)
     if (session) {
       session.end().then(() => {
-        route.push('/laboratory')
+        route.prefetch('/laboratory')
         setIsOpen(!isOpen)
         return;
       })
