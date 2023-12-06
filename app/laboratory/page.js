f6680fdf7637993c9f3d7478848643cdd98293ff
feri <casterjam56@gmail.com>
2023-12-06 10:10:13 +0800

    add global context

--- app/laboratory/page.js
+++ app/laboratory/page.js
@@ -1,16 +1,22 @@
 "use client"
 
 import dynamic from 'next/dynamic'
-import { Suspense } from 'react'
+import { Suspense, useContext, useEffect } from 'react'
 import { OrbitControls, PerspectiveCamera, PointerLockControls } from '@react-three/drei'
 import LabkomE from '@/components/rooms/labkome'
 import Wrapper from '@/components/utils/wrapper'
 import { SimulationLight } from '@/components/lighting/light'
+import { ContextData } from '@/components/utils/context'
 
 const Adam = dynamic(() => import('@/components/model/Adam').then(mod => mod.Adam))
 const Views = dynamic(() => import('@/components/canvas/views'), { ssr: false })
 
 export default function Laboratory() {
+  const myContext = useContext(ContextData)
+
+  useEffect(() => {
+    console.log(myContext.status)
+  })
   return (
     <div className='w-full h-full bg-white relative'>
       <Views styling='w-full h-full'>
--- components/templates/template.js
+++ components/templates/template.js
@@ -4,14 +4,17 @@ import { useRef } from "react"
 import Scene from "../canvas/scene"
 import { ToastContainer } from "react-toastify"
 import "react-toastify/dist/ReactToastify.css"
+import { ContextData } from "../utils/context"
 
 export default function Template({ children }) {
   const parent = useRef(null)
   return (
     <div ref={parent} className='parent w-full h-screen relative'>
-      {children}
+      <ContextData.Provider value={{ status: 'fps' }}>
+        {children}
+      </ContextData.Provider>
       <Scene source={parent} />
-      <ToastContainer position='bottom-left' hideProgressBar style={{zIndex: 10000}} />
+      <ToastContainer position='bottom-left' hideProgressBar style={{ zIndex: 10000 }} />
     </div>
   )
 }
