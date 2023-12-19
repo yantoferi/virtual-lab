082eb8904893c2ee02a228c77deca97195e35d3e
feri <casterjam56@gmail.com>
2023-12-19 23:33:28 +0800

    change shadow light

--- components/lighting/light.js
+++ components/lighting/light.js
@@ -7,7 +7,7 @@ function SimulationLight(props) {
   useHelper(light, DirectionalLightHelper, 2, 'blue')
   return (
     <>
-      <directionalLight ref={light} color='yellow' intensity={2} position={props.position} castShadow shadow-mapSize={[2048, 2048]} shadow-bias={-0.0001}>
+      <directionalLight ref={light} color='yellow' intensity={3} position={props.position} castShadow shadow-mapSize={[4096, 4096]} shadow-bias={-0.0001}>
         <orthographicCamera args={[-50, 50, 50, -50]} attach='shadow-camera' />
         <object3D position={props.targetPos} attach='target' />
       </directionalLight>
